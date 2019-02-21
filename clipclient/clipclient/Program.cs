5bf5523cc4bd4484b28339a220a7c7734af8935a
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-02-21 14:36:28 +0530

    cleanup

--- clipclient/clipclient/Program.cs
+++ clipclient/clipclient/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Forms;
+using System.Net.Http;
 using Keystroke.API;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace clipclient
 {
@@ -17,20 +18,37 @@ namespace clipclient
         static void Main(string[] args)
         {
             string ClipboardContent = "";
+            string API_URL = "http://localhost:8888/";
+
+            HttpClient client = new HttpClient();
 
             using (var api = new KeystrokeAPI())
             {
-                api.CreateKeyboardHook((character) => {
-                Console.Write(character);
+                api.CreateKeyboardHook(async (character) => {
+                    Console.Write(character);
 
-                if (Clipboard.ContainsText())
-                {
-                    if (ClipboardContent != Clipboard.GetText())
+                    if (Clipboard.ContainsText())
                     {
-                        ClipboardContent = Clipboard.GetText();
+                        if (ClipboardContent != Clipboard.GetText())
+                        {
+                            ClipboardContent = Clipboard.GetText();
                             Console.WriteLine(ClipboardContent);
+
+                            object data = new
+                            {
+                                content = ClipboardContent 
+                            };
+
+
+                            var content = new StringContent(JsonConvert.SerializeObject(data), UnicodeEncoding.UTF8, "application/json");
+                            var response = await client.PostAsync(API_URL + "uploadclip", content);
+                            var result = await response.Content.ReadAsStringAsync();
+
+                            Console.WriteLine(result);
+
                         }
                     }
+
                 });
                 //This call starts the windows message loop for you, but you will need to reference the 
                 //System.Windows.Forms.dll in your project.
